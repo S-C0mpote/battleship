eae2ec4387114443229a32c8ff2d60b094cf1afc
E205687Y <morgann.leray@etu.univ-nantes.fr>
2021-06-21 13:59:01 +0200

    GameObject interface added

--- src/info1/game/engine/GameObject.java
+++ src/info1/game/engine/GameObject.java
@@ -1,4 +1,24 @@
 package info1.game.engine;
 
+import java.awt.*;
+
 public interface GameObject {
+    /*
+    *
+    *
+    */
+    void update(double delta);
+
+    /*
+    * Appellé quand on doit dessiner le game object
+    * @param g2d, le truc pour dessiner je suppose
+    */
+    void draw(Graphics2D g2d);
+
+    /*
+    * Initialise la scène donnée
+    * @param engine la scène à initialiser
+    */
+    void init(GameEngine engine);
+
 }
