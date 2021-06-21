d5f7287fe94328c801edaf92cc1509977f293466
E201948J <elouan.naquin@etu.univ-nantes.fr>
2021-06-21 13:52:03 +0200

    Enum Scenes

--- src/info1/game/engine/Scenes.java
+++ src/info1/game/engine/Scenes.java
@@ -1,4 +1,15 @@
 package info1.game.engine;
 
 public enum Scenes {
+    MENU(new Scene("Menu")), IN_GAME(new Scene("Jeu"));
+
+    private Scene scene;
+
+    Scenes(Scene scene){
+        this.scene = scene;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
 }
