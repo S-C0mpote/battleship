18ce08d1701cf88bc53be94de2e8541c2ac72d03
E201948J <elouan.naquin@etu.univ-nantes.fr>
2021-06-25 00:04:40 +0200

    Animation part 1

--- src/info1/game/engine/gameobjects/StartAnimation.java
+++ src/info1/game/engine/gameobjects/StartAnimation.java
@@ -23,35 +23,34 @@ public class StartAnimation extends GameObject {
     @Override
     public void update(double delta) {
         if(animated) {
-            if(leftPart.y >= 720 / 2d - 300) {
-                System.out.println("condition 1 : " + leftPart.y);
-
-                if(leftPart.y < 720 / 2d && leftPart.y > 720 / 2d - 20){
-                    leftPart.y += delta * (delta/ vitesse);
-                    vitesse *= 1.00001 ;
-                }else{
-                    leftPart.y += delta * 1.005;
-                    if(leftPart.y >= 720 / 2d) leftPart.x -= vitesse / delta;
+            if(leftPart.y == 720 / 2d - 300) {
+                if (leftPart.x > -1280 + 640 - 10) {
+                    leftPart.x += delta * (delta / vitesse2);
+                    vitesse2 *= 1.001;
+                } else {
+                    leftPart.x += vitesse2 / delta;
                 }
 
-                if(leftPart.x <= -1280) {
+                if (leftPart.x > -1280 / 2d) {
+                    leftPart.y++;
+                }
+            }else if(leftPart.y > 720 / 2d - 300){
+
+                if (leftPart.y < 720 / 2d && leftPart.y > 720 / 2d - 20) {
+                    leftPart.y += delta * (delta / vitesse);
+                    vitesse *= 1.00001;
+                } else {
+                    leftPart.y += delta * 1.005 * 1.5;
+                    if (leftPart.y >= 720 / 2d) {
+                        leftPart.x -= (delta / 2) * 1.5;
+
+                    }
+                }
+
+                if (leftPart.x <= -1280) {
                     leftPart.x = -1280;
                     animated = false;
                     part = 1;
-                }
-            }else{
-                System.out.println("condition 2 : " + leftPart.y);
-                if(leftPart.y == 720 / 2d - 300) {
-                    if (leftPart.x > -1280 + 640 - 10) {
-                        leftPart.x += delta * (delta / vitesse2);
-                        vitesse2 *= 1.001;
-                    } else {
-                        leftPart.x += vitesse2 / delta;
-                    }
-
-                    if (leftPart.x > -1280 / 2d) {
-                        leftPart.x = -1280 / 2d;
-                    }
                 }
             }
         }
